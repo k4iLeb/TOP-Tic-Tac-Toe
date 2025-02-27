347bb7c2d7f2fb6829a9153802cca1bbf235f5b4
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-02-27 02:43:12 +0200

    Create gameBoard module & createPlayer factory

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("asdf");
+// *** GAMEBOARD ***
+const gameBoard = (() => {
+  const board = [, , , , , , , , ,];
+  const setMark = (index, player) => {
+    board[index] = player;
+  };
+  return { board, setMark };
+})();
+
+// *** PLAYERS ***
+function createPlayer(name, marker) {
+  return { name, marker };
+}
+
+const p1 = createPlayer("p1", "x");
+
+const p2 = createPlayer("p2", "o");
+// console.log(p1);
+
+// *** GAME LOGIC ***
+function checkGame() {
+  const board = gameBoard.board;
+  console.log(board);
+  // console.log(board[0].name);
+
+  if (board[1] === board[4] && board[4] === board[7]) {
+    console.log(`${board[1].name} WINS`);
+  }
+
+  // USE FOR LOOP TO CHECK
+}
+
+gameBoard.setMark(1, p1);
+gameBoard.setMark(4, p1);
+gameBoard.setMark(7, p1);
+checkGame();
